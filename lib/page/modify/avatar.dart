51ad1a6c086303695169f914cb24a0984c744b4a
xiaohuli <waterdoor@foxmail.com>
2019-11-25 11:04:48 +0800

    fix: 裁剪图片

--- lib/page/modify/avatar.dart
+++ lib/page/modify/avatar.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../../global.dart';
-import 'package:image_crop/image_crop.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:image_cropper/image_cropper.dart';
+import 'dart:io';
+import 'dart:convert';
 
 class Avatar extends StatefulWidget {
   Avatar({Key key, @required this.handler, @required this.modifyFunc}) 
@@ -17,20 +19,28 @@ class Avatar extends StatefulWidget {
 class _AvatarState extends State<Avatar> {
   var _imgPath;
   bool showCircle = false;
-  final cropKey = GlobalKey<CropState>();
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: <Widget>[
         SingleChildScrollView(child: imageView(context),) ,
-        RaisedButton(
-          onPressed: () => pickImg('takePhote'),
-          child: Text('拍照')
-        ),
-        RaisedButton(
-          onPressed: () => pickImg('gallery'),
-          child: Text('选择相册')
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            RaisedButton(
+              onPressed: () => pickImg('takePhote'),
+              child: Text('拍照')
+            ),
+            RaisedButton(
+              onPressed: () => pickImg('gallery'),
+              child: Text('选择相册')
+            ),
+            RaisedButton(
+              onPressed: getBase64,
+              child: Text('获取base64')
+            )
+          ],
         )
       ],
     );
@@ -43,21 +53,13 @@ class _AvatarState extends State<Avatar> {
       );
     } else if (_imgPath != null) {
       return Center(
-          child: Container(
-              color: Colors.black,
-              padding: const EdgeInsets.all(20.0),
-              child: Crop(
-                key: cropKey,
-                image: FileImage(_imgPath),
-                aspectRatio: 4.0 / 3.0,
-              ),
+          child: 
+          FadeInImage(
+            placeholder: AssetImage("images/loading.gif"),
+            image: FileImage(_imgPath),
+            height: 375,
+            width: 375,
           )
-          // FadeInImage(
-          //   placeholder: AssetImage("images/loading.gif"),
-          //   image: FileImage(_imgPath),
-          //   height: 375,
-          //   width: 375,
-          // )
       ); 
     } else {
       return Center(
@@ -67,6 +69,14 @@ class _AvatarState extends State<Avatar> {
         )
       );
     }
+  }
+
+  void getBase64() async {
+    print(_imgPath.path);
+    File file = new File(_imgPath.path);
+    List<int> imageBytes = await file.readAsBytes();
+    print(base64Encode(imageBytes).toString());
+    //return base64Encode(imageBytes);
   }  
 
   void pickImg(String action) async{
@@ -75,9 +85,27 @@ class _AvatarState extends State<Avatar> {
       showCircle = true;
     });
     var image = await (action == 'gallery' ? ImagePicker.pickImage(source: ImageSource.gallery) : ImagePicker.pickImage(source: ImageSource.camera));
+    File croppedFile = await ImageCropper.cropImage(
+      sourcePath: image.path,
+      aspectRatio: CropAspectRatio(ratioX: 1, ratioY:1),
+      compressQuality: 60,
+      maxHeight: 100,
+      maxWidth: 100,
+      compressFormat: ImageCompressFormat.jpg,
+      aspectRatioPresets: [
+        CropAspectRatioPreset.square,
+      ],
+      androidUiSettings: AndroidUiSettings(
+        toolbarTitle: 'Cropper',
+        toolbarColor: Colors.blue,
+        toolbarWidgetColor: Colors.white,
+        initAspectRatio: CropAspectRatioPreset.original,
+        lockAspectRatio: false
+      )
+    );
     setState(() {
       showCircle = false;
-      _imgPath = image;
+      _imgPath = croppedFile;
     });
   }
 }
